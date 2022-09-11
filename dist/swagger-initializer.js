e7ac9e4d99a1c1f2b5b78afa7cada5a79d776e86
fulfilnet <tech.be@fulfilnet.co>
2022-09-12 07:15:35 +0800

    update config

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "swagger.yaml",
+    url: "https://api-staging.fulfilnet.co/open_api/swagger_doc",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
